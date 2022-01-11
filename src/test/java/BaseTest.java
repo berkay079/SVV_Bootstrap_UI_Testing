ea99023474cb15bf81e5c3d4f76525b1b188cbb6
berkay <berkayyildirim871@gmail.com>
2022-01-11 14:50:06 +0300

    add_signUp_test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -56,15 +57,14 @@ public class BaseTest {
         SearchTest searchTest = new SearchTest();
         searchTest.searchKeyword("Download",driver);
 
+        SignUp signUp = new SignUp();
+        signUp.signUp(driver);
     }
 
 
-
-
-
-   /* @AfterMethod
+    @AfterMethod
     public void tearDown() {
         driver.quit();
-    }*/
+    }
 
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -4,14 +4,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class SearchTest extends BaseTest {
 
     @Test
     public void searchKeyword(String keyword, WebDriver driver) throws InterruptedException {
-
         WebElement searchInput = driver.findElement(By.id("search-input"));
         searchInput.sendKeys(keyword);
 
@@ -20,8 +18,5 @@ public class SearchTest extends BaseTest {
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("algolia-autocomplete-listbox-0")));
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".algolia-docsearch-suggestion__main"))).click();
 
-
-
     }
-
 }
